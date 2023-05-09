8b7eb71a7c250731339a97a901bfca1640ef754a
vinaykanna <48582080+vinaykanna@users.noreply.github.com>
2023-05-09 12:24:39 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-5xl">Form Builder Project</h1>
+      <h1 className="text-5xl">Development in progresst</h1>
     </main>
   );
 }
